097e5b0246f26edd6c55b01ab299acefc7642329
bgray <benjamin.gray@yahoo.com>
2023-03-15 07:07:24 -0400

    outputs ninja list conditionally using if statement or a ternary operator

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
 
   state = {
     ninjas: [
-      { name: 'Ryu', age: '30', belt: 'black', id: 1 },
+      { name: 'Ryu', age: '35', belt: 'black', id: 1 },
       { name: 'Yoshi', age: '20', belt: 'green', id: 2 },
       { name: 'Crystal', age: '25', belt: 'pink', id: 3 }
     ]
--- src/Ninjas.js
+++ src/Ninjas.js
@@ -2,15 +2,31 @@ import React from 'react';
 
 const Ninjas = ({ ninjas }) => {
         
+    // const ninjaList = ninjas.map(ninja => {
+    //     if (ninja.age > 20) {
+    //         return (
+    //             <div className='ninja' key={ninja.id}>
+    //                 <div>Name: { ninja.name }</div>
+    //                 <div>Age: { ninja.age }</div>
+    //                 <div>Belt: { ninja.belt }</div>
+    //             </div>
+    //         )
+    //     } else {
+            // return null;
+            // }
+    // })
+
     const ninjaList = ninjas.map(ninja => {
-        return (
+        return ((ninja.age > 30) ? (
             <div className='ninja' key={ninja.id}>
                 <div>Name: { ninja.name }</div>
                 <div>Age: { ninja.age }</div>
                 <div>Belt: { ninja.belt }</div>
             </div>
-        )
-    })
+        ) : (
+            null
+        ))
+    });
 
     return (
         <div className='ninja-list'>
