594e052e2ac2ebada3f229647491db9767722062
bgray <benjamin.gray@yahoo.com>
2023-03-15 06:55:07 -0400

    completes chapter 15; adds state to App component; nests Ninja component in App; updates Ninja component to a functional component; destructures {ninjas from props in Ninja component}

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
+class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <h1>My First React App</h1>
-      <p>Welcome!</p>
-    </div>
-  );
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: '30', belt: 'black', id: 1 },
+      { name: 'Yoshi', age: '20', belt: 'green', id: 2 },
+      { name: 'Crystal', age: '25', belt: 'pink', id: 3 }
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>My First React App</h1>
+        <p>Welcome!</p>
+        <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    );
+  }
 }
 
 export default App;
